cf539178ff6abf6504c20326ec33ba3562fcdaa3
libnipineda <libnipineda@hotmail.com>
2024-06-16 21:54:23 -0600

    [ADD]Creacion server nodejs

---
+++ WindowsApp1/Backend/src/routes/index.js
@@ -0,0 +1,12 @@
+﻿const { Router } = require('express');
+const router = Routre();
+
+
+// Raiz
+router.get('/', (req, res) => {
+    res.json({
+        "Title": "Bienvenido al servidor de Node JS!!!!"
+    });
+})
+
+module.exports = router;
