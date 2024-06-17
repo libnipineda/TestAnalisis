5da7ad6673a9e1e4e3cc58a045a093c031cb7dcb
libnipineda <libnipineda@hotmail.com>
2024-06-16 18:28:04 -0600

    [ADD]Obtener datos

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -2,7 +2,7 @@
 
 Public Class Form1
 
-    Dim cod As Integer
+    Dim cod As Int64
     Dim desc As String
     Dim fecha As Date
     Dim status As String
@@ -26,8 +26,33 @@ Public Class Form1
         End If
     End Sub
 
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Char.IsDigit(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         VerificarDatos()
 
+        cod = CULng(TextBox1.Text)
+        desc = RichTextBox1.Text
+        status = TextBox2.Text
+        origen = TextBox3.Text
+
+        Console.WriteLine(cod)
+        Console.WriteLine(desc)
+        Console.WriteLine(status)
+        Console.WriteLine(origen)
+
+        MessageBox.Show("Datos ingresados de forma Correcta", "Informacion")
+
+
     End Sub
+
 End Class
